14fe357e598e6e02cfc1f31126b974d39ed8516b
schrmtmr <kiw.officetyo@gmail.com>
2026-02-27 15:10:45 +0900

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ export const CONFIG = {
   // テーマファイルが配置されているベースURL
   THEME_BASE_URL: 'https://raw.githack.com/schrmtmr/marp-theme/main/themes',
   
-  // デフォルトのテーマファイル名
-  DEFAULT_THEME: 'main.css',
+  // デフォルトのテーマ名（拡張子なしで定義）
+  DEFAULT_THEME_NAME: 'main',
   
   // DOM上で設定を読み取るためのトリガー画像のセレクタ
   TRIGGER_SELECTOR: 'img[data-hook="marp-style"]',
